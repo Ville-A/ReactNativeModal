0c64e35cade915730d29af6b39afe0cd5e903d39
Ville-A <ville.m.ahola@gmail.com>
2024-09-30 19:17:41 +0300

    Added expo modal app

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Modal, Text, Pressable, View, StyleSheet, BackHandler } from 'react-native';
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    const backAction = () => {
+      if (visible) {
+        setVisible(false);
+        return true;
+      }
+      return false;
+    };
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+    return () => backHandler.remove(); 
+  }, [visible]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable style={styles.pressable} onPress={() => setVisible(true)}>
+        <Text style={styles.textStyle}>Show modal message</Text>
+      </Pressable>
+      <Modal visible={visible} onRequestClose={() => {setVisible(false);}}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>This is modal...</Text>
+            <Pressable style={[styles.button, styles.buttonClose]} onPress={() => setVisible(false)}>
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -13,8 +37,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  pressable: {
+    padding: 10,
+    backgroundColor: '#75b4c7',
+    borderRadius: 10,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)', 
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+## Expo app
+
+# What the app looks like normally and after you press close.
+
+![App Screenshot](./assets/modal.PNG)
+
+# What the app looks like when you press the Modal.
+
+![App Screenshot when Modal is pressed](./assets/modalPressed.PNG)
